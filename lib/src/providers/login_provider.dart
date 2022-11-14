4af3d433edc98b6cc80e83af5bbabbc3b600ba27
Luis Felipe Santofimio Buitrago <luisfelipesantofimio@gmail.com>
2022-11-13 23:30:24 -0500

    Implemented remote query for professional login

--- lib/src/providers/login_provider.dart
+++ lib/src/providers/login_provider.dart
@@ -1,6 +1,36 @@
+import 'dart:developer';
+
+import 'package:aronnax/src/API/server_api.dart';
 import 'package:aronnax/src/database/local_model/local_model.dart';
 import 'package:aronnax/src/database/local_model/local_queries.dart';
+import 'package:aronnax/src/database/models/remote_professional.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class GlobalRemoteLoginState extends StateNotifier<List<RemoteProfessional>> {
+  GlobalRemoteLoginState() : super([]);
+  var db = MySQL();
+  void getProfessionalsInServer() {
+    state.clear();
+    db.getConnection().then((conn) {
+      String loginQuery = "select * from professional";
+      //"select * from professional where personalID = $personalID";
+
+      conn.query(loginQuery).then((result) {
+        log(result.toString());
+        for (var element in result) {
+          state.add(
+            RemoteProfessional.fromJson(element.fields),
+          );
+        }
+      });
+    });
+  }
+}
+
+final remoteLoginStateProvider =
+    StateNotifierProvider<GlobalRemoteLoginState, List<RemoteProfessional>>(
+  (ref) => GlobalRemoteLoginState(),
+);
 
 final localLoginStateProvider =
     StreamProvider.family<List<ProfessionalData>, int>(
