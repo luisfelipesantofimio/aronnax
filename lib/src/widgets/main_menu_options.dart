d953bb92bc0eb488e1918ee9e190d78edb36a2ee
Luis Felipe Santofimio Buitrago <luisfelipesantofimio@gmail.com>
2022-12-11 19:51:05 -0500

    Removed unnecessary import

--- lib/src/widgets/main_menu_options.dart
+++ lib/src/widgets/main_menu_options.dart
@@ -1,5 +1,4 @@
 import 'package:aronnax/src/Pages/ClinicHistory/clinic_history_search.dart';
-import 'package:aronnax/src/Pages/Consultations/main_consult/consultation_view.dart';
 import 'package:aronnax/src/Pages/Formulary/basic_form/basic_form_view.dart';
 import 'package:aronnax/src/Pages/Formulary/clinic_history/clinic_history_form_view.dart';
 import 'package:aronnax/src/Pages/Formulary/session/session_form_view.dart';
