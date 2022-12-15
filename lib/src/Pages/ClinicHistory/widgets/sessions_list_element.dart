0bd22f5e0167baa0a9152aa7d7ebdb0d4be265a2
Luis Felipe Santofimio Buitrago <luisfelipesantofimio@gmail.com>
2022-12-14 20:58:58 -0500

    Added new sessions display items

---
+++ lib/src/Pages/ClinicHistory/widgets/sessions_list_element.dart
@@ -0,0 +1,156 @@
+import 'package:aronnax/src/misc/global_values.dart';
+import 'package:flutter/material.dart';
+
+class SessionsListElement extends StatelessWidget {
+  const SessionsListElement(
+      {Key? key,
+      required this.sessionSummary,
+      required this.sessionArchievements,
+      required this.sessionObjectives,
+      required this.sessionID,
+      required this.sessionDate,
+      required this.sessionProfessional,
+      required this.itemIndex})
+      : super(key: key);
+  final int sessionID;
+  final String sessionDate;
+  final String sessionProfessional;
+  final String sessionSummary;
+  final String sessionArchievements;
+  final String sessionObjectives;
+  final int itemIndex;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(
+          Radius.circular(
+            5,
+          ),
+        ),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey,
+            offset: Offset(0, 3),
+            blurRadius: 3,
+          ),
+        ],
+      ),
+      width: MediaQuery.of(context).size.width,
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  sessionDate,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  sessionProfessional,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            const Padding(
+              padding: EdgeInsets.all(
+                15,
+              ),
+            ),
+            const Text(
+              "Resumen de la sesión:",
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(
+                5,
+              ),
+            ),
+            Text(
+              sessionSummary,
+              style: const TextStyle(
+                color: Colors.black,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(
+                5,
+              ),
+            ),
+            const Text(
+              "Logros terapéuticos:",
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(
+                5,
+              ),
+            ),
+            Text(
+              sessionArchievements,
+              style: const TextStyle(
+                color: Colors.black,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(
+                5,
+              ),
+            ),
+            const Text(
+              "Objetivos de la sesión:",
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(
+                5,
+              ),
+            ),
+            Text(
+              sessionObjectives,
+              style: const TextStyle(
+                color: Colors.black,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(
+                5,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  "${itemIndex + 1}",
+                  style: const TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/Pages/ClinicHistory/widgets/sessions_list_view.dart
@@ -0,0 +1,46 @@
+import 'package:aronnax/src/Pages/ClinicHistory/widgets/sessions_list_element.dart';
+import 'package:aronnax/src/Pages/LoginScreen/login_form.dart';
+import 'package:aronnax/src/database/local_model/local_model.dart';
+import 'package:aronnax/src/database/models/remode_session_resume.dart';
+import 'package:flutter/material.dart';
+
+class SessionsListView extends StatelessWidget {
+  const SessionsListView(
+      {Key? key, required this.remoteSessions, required this.localSessions})
+      : super(key: key);
+
+  final List<RemoteSession> remoteSessions;
+  final List<Session> localSessions;
+
+  @override
+  Widget build(BuildContext context) {
+    return DraggableScrollableSheet(
+      initialChildSize: 1,
+      maxChildSize: 1,
+      minChildSize: 0.1,
+      expand: true,
+      builder: (context, scrollController) {
+        return ListView.builder(
+          controller: scrollController,
+          itemCount:
+              isOfflineEnabled ? localSessions.length : remoteSessions.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SessionsListElement(
+                itemIndex: index,
+                sessionSummary: remoteSessions[index].sessionSummary,
+                sessionArchievements:
+                    remoteSessions[index].therapeuticArchievements,
+                sessionObjectives: remoteSessions[index].sessionObjectives,
+                sessionID: remoteSessions[index].sessionId,
+                sessionDate: remoteSessions[index].dateTime,
+                sessionProfessional: remoteSessions[index].professionalName,
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
