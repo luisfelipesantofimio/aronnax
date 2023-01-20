d24668d300d33b7a9e149b1430aebb4e2866e5c4
Luis Felipe Santofimio Buitrago <luisfelipesantofimio@gmail.com>
2023-01-19 23:10:20 -0500

    Moved repositories location in data layer

--- lib/src/domain/repositories/auth_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:crypt/crypt.dart';
-
-class AuthRepository {
-  bool loginUser(String obtainedPassword, userPassword) {
-    return Crypt(obtainedPassword).match(userPassword);
-  }
-}
