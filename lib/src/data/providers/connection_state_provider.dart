8dcf74541dccbd4f4d89a55590d65645427d2b7f
Luis Felipe Santofimio <luisfelipesantofimio@gmail.com>
2023-02-09 23:43:24 -0500

    Implemented gloabal connection provider

--- lib/src/data/providers/connection_state_provider.dart
+++ lib/src/data/providers/connection_state_provider.dart
@@ -2,27 +2,10 @@ import 'package:aronnax/src/data/database/local_model/local_model.dart';
 import 'package:aronnax/src/data/interfaces/local_database_interface.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class GlobalOfflineStatus extends StateNotifier<bool> {
-  GlobalOfflineStatus() : super(false);
-
-  // void getConnectionStatus() async {
-  //   state = currentSettings!.isOfflineModeEnabled!;
-  // }
-
-  // void updateCurrentStatus() async {
-  //   Settings? currentSettings = await settings.get(0);
-  //   currentSettings!.isDarkModeEnabled = !currentSettings.isDarkModeEnabled!;
-  //   settings.put(currentSettings);
-  // }
-}
-
 final offlineStatusProvider = FutureProvider<bool>((ref) async {
   Setting settings =
       await ref.watch(localDatabaseRepositoryProvider).getLocalSettings();
   return settings.isOfflineModeEnabled;
 });
 
-final globalOfflineStatusProvider =
-    StateNotifierProvider<GlobalOfflineStatus, bool>(
-  (ref) => GlobalOfflineStatus(),
-);
+final globalOfflineStatusProvider = StateProvider((ref) => false);
