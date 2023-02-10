c00cf0e4116fb594fc09b33b892ed0c181619966
Luis Felipe Santofimio <luisfelipesantofimio@gmail.com>
2023-02-09 23:38:29 -0500

    Modified queries

--- lib/src/data/database/local_model/local_model.dart
+++ lib/src/data/database/local_model/local_model.dart
@@ -1,121 +1,11 @@
 import 'dart:io';
+import 'package:aronnax/src/data/database/local_model/tables.dart';
 import 'package:drift/native.dart';
 import 'package:path/path.dart' as p;
 import 'package:drift/drift.dart';
 import 'package:path_provider/path_provider.dart';
 
 part 'local_model.g.dart';
-
-class Patients extends Table {
-  IntColumn get id => integer().autoIncrement()();
-  TextColumn get names => text()();
-  TextColumn get lastNames => text()();
-  IntColumn get idNumber => integer().unique()();
-  DateTimeColumn get birthDate => dateTime()();
-  IntColumn get contactNumber => integer()();
-  TextColumn get mail => text()();
-  TextColumn get city => text()();
-  TextColumn get state => text()();
-  TextColumn get adress => text()();
-  TextColumn get education => text()();
-  TextColumn get ocupation => text()();
-  TextColumn get insurance => text()();
-  TextColumn get emergencyContactName => text()();
-  IntColumn get emergencyContactNumber => integer()();
-  DateTimeColumn get creationDate => dateTime()();
-  BoolColumn get isActive => boolean()();
-  IntColumn get professionalID =>
-      integer().references(Professional, #professionalID)();
-}
-
-class ClinicHistory extends Table {
-  IntColumn get id => integer().autoIncrement()();
-  TextColumn get registerNumber => text()();
-  DateTimeColumn get currentDate => dateTime()();
-  TextColumn get consultationReason => text()();
-  TextColumn get mentalExamination => text()();
-  TextColumn get treatment => text()();
-  TextColumn get medAntecedents => text()();
-  TextColumn get psiAntecedents => text()();
-  TextColumn get familyHistory => text()();
-  TextColumn get personalHistory => text()();
-  TextColumn get diagnostic => text()();
-  IntColumn get idNumber => integer().references(Patients, #idNumber)();
-  IntColumn get professionalID =>
-      integer().references(Professional, #professionalID)();
-}
-
-class Sessions extends Table {
-  IntColumn get sessionId => integer().autoIncrement()();
-  DateTimeColumn get sessionDate => dateTime()();
-  TextColumn get sessionSummary => text()();
-  TextColumn get sessionObjectives => text()();
-  TextColumn get therapeuticArchievements => text()();
-  TextColumn get sessionNotes => text()();
-  TextColumn get sessionPerformance => text()();
-  IntColumn get idNumber => integer().references(Patients, #idNumber)();
-  IntColumn get professionalID =>
-      integer().references(Professional, #professionalID)();
-}
-
-class Professional extends Table {
-  IntColumn get id => integer().autoIncrement()();
-  IntColumn get personalID => integer().unique()();
-  TextColumn get names => text()();
-  TextColumn get lastNames => text()();
-  IntColumn get professionalID => integer()();
-  TextColumn get userName => text()();
-  TextColumn get password => text()();
-}
-
-class Tests extends Table {
-  IntColumn get id => integer().autoIncrement()();
-  DateTimeColumn get testDate => dateTime()();
-  IntColumn get patientID => integer().references(Patients, #idNumber)();
-  IntColumn get professionalID =>
-      integer().references(Professional, #professionalID)();
-  IntColumn get sessionID => integer().references(Sessions, #sessionId)();
-  TextColumn get testDescription => text()();
-  TextColumn get category => text()();
-  TextColumn get testData => text()();
-}
-
-class Todos extends Table {
-  IntColumn get id => integer().autoIncrement()();
-  DateTimeColumn get creationDate => dateTime()();
-  TextColumn get todo => text()();
-  BoolColumn get isComplete => boolean()();
-  IntColumn get sessionID => integer().references(Sessions, #sessionId)();
-}
-
-class Appointments extends Table {
-  IntColumn get id => integer().autoIncrement()();
-  DateTimeColumn get date => dateTime()();
-  IntColumn get professionalID =>
-      integer().references(Professional, #professionalID)();
-  IntColumn get patientID => integer().references(Patients, #idNumber)();
-  TextColumn get description => text()();
-  TextColumn get status => text()();
-  TextColumn get sessionType => text()();
-}
-
-class Settings extends Table {
-  IntColumn get id => integer()();
-  BoolColumn get isDarkModeEnabled => boolean()();
-  BoolColumn get isOfflineModeEnabled => boolean()();
-  BoolColumn get isConfigured => boolean()();
-  @override
-  Set<Column> get primaryKey => {id};
-}
-
-class ServerDatabase extends Table {
-  IntColumn get id => integer().autoIncrement()();
-  TextColumn get userName => text()();
-  TextColumn get password => text()();
-  TextColumn get server => text()();
-  IntColumn get port => integer()();
-  TextColumn get databaseName => text()();
-}
 
 @DriftDatabase(tables: [
   Patients,
@@ -171,7 +61,7 @@ class LocalDatabase extends _$LocalDatabase {
   Future updateConnectionMode(bool currentConnectionMode) async {
     return (update(settings)..where((t) => t.id.equals(0))).write(
       SettingsCompanion(
-        isOfflineModeEnabled: Value(!currentConnectionMode),
+        isOfflineModeEnabled: Value(currentConnectionMode),
       ),
     );
   }
@@ -192,6 +82,10 @@ class LocalDatabase extends _$LocalDatabase {
             (tbl) => tbl.names.contains(userNames),
           ))
         .watch();
+  }
+
+  Future<List<ProfessionalData>> getProfessionalsList() {
+    return (select(professional)).get();
   }
 
   Future<List<ProfessionalData>> loginProfessional(int userID) {
@@ -221,10 +115,6 @@ class LocalDatabase extends _$LocalDatabase {
         .get();
   }
 
-  Future<List<ProfessionalData>> isProfessionalsListEmpty() async {
-    return (select(professional)).get();
-  }
-
   Future<Setting?> verifyExistingSettings() {
     return (select(settings)..where((tbl) => tbl.id.equals(0)))
         .getSingleOrNull();
@@ -234,11 +124,15 @@ class LocalDatabase extends _$LocalDatabase {
     return (select(settings)..where((tbl) => tbl.id.equals(0))).getSingle();
   }
 
-  Future updateConfigurationState(Setting state, bool isOffline) {
+  Future<List<ServerDatabaseData>> getServerConfigurations() {
+    return (select(serverDatabase)).get();
+  }
+
+  Future updateConfigurationState(bool isConfigured) {
     return (update(settings)..where((t) => t.id.equals(0))).write(
       SettingsCompanion(
-          isConfigured: Value(!state.isConfigured),
-          isOfflineModeEnabled: Value(isOffline)),
+        isConfigured: Value(isConfigured),
+      ),
     );
   }
 }
