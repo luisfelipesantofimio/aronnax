e120d89dfa6cb540216ab9eed7cdb1c3bfcd1405
Luis Felipe Santofimio Buitrago <luisfelipesantofimio@gmail.com>
2023-02-19 22:54:56 -0500

    Implemented new routes

--- lib/src/presentation/main_menu/main_menu_options.dart
+++ lib/src/presentation/main_menu/main_menu_options.dart
@@ -1,8 +1,8 @@
 import 'package:aronnax/src/Pages/ClinicHistory/clinic_history_search.dart';
-import 'package:aronnax/src/Pages/Formulary/session/session_form_view.dart';
 import 'package:aronnax/src/Pages/Formulary/widgets/consultant_selection_dialog.dart';
 import 'package:aronnax/src/presentation/clinic_history_form_screen/clinic_history_register_view.dart';
 import 'package:aronnax/src/presentation/register_view/register_view.dart';
+import 'package:aronnax/src/presentation/session_creation_view/session_form_view.dart';
 import 'package:aronnax/src/presentation/widgets/main_options_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -67,7 +67,7 @@ class MainMenuOptions extends ConsumerWidget {
                         context: context,
                         builder: (context) => const ConsultantSelectionDialog(
                           title: "Selecciona al consultante",
-                          destinationRoute: MainViewSession(),
+                          destinationRoute: SessionFormView(),
                         ),
                       );
                     }),
