048687077ac056db69d0f494fb60657f5298e20b
Luis Felipe Santofimio <luisfelipesantofimio@gmail.com>
2023-02-22 20:58:24 -0500

    Implemented data model for common use

---
+++ lib/src/domain/entities/calendar_event.dart
@@ -0,0 +1,44 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:aronnax/src/data/database/local_model/local_model.dart';
+import 'package:aronnax/src/presentation/core/methods.dart';
+
+enum CalendarEventStates {
+  scheduled,
+  cancelled,
+  done,
+}
+
+enum CalendarEventType {
+  consultation,
+  meeting,
+  following,
+}
+
+class CalendarEvent {
+  final DateTime date;
+  final CalendarEventStates state;
+  final CalendarEventType eventType;
+  final String? description;
+  final int professionalID;
+  final int patientID;
+  CalendarEvent({
+    required this.date,
+    required this.state,
+    required this.eventType,
+    required this.description,
+    required this.professionalID,
+    required this.patientID,
+  });
+
+  factory CalendarEvent.fromLocalModel(LocalAppointment data) {
+    return CalendarEvent(
+      patientID: data.patientID,
+      professionalID: data.professionalID,
+      date: data.date,
+      state: AppMethods().parseCalendarEventStateFromString(data.status),
+      description: data.description,
+      eventType:
+          AppMethods().parseCalendarEventTypeFromString(data.sessionType),
+    );
+  }
+}
