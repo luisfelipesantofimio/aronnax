799b5dfc8b96e816c0e1513c526188258674b6b7
Luis Felipe Santofimio Buitrago <luisfelipesantofimio@gmail.com>
2023-02-26 22:07:48 -0500

    Implemented future provider for todos reads and updated

--- lib/src/data/interfaces/todos_repository_interface.dart
+++ lib/src/data/interfaces/todos_repository_interface.dart
@@ -1,8 +1,15 @@
+import 'package:aronnax/src/data/repositories/todos_repository.dart';
 import 'package:aronnax/src/domain/entities/todo.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 abstract class TodosRepositoryInterface {
-  Future<List<Todo>> getTodosList(WidgetRef ref);
+  Future<List<Todo>> getTodosList(Ref ref);
 
-  void deleteTodo(WidgetRef ref, int todoId);
+  Future<void> deleteTodo(WidgetRef ref, int todoId);
+
+  Future<void> updateTodoDoneState(WidgetRef ref, int todoId, bool newState);
 }
+
+final todosRepositoryProvider = Provider<TodosRepositoryInterface>(
+  (ref) => TodosRepository(),
+);
