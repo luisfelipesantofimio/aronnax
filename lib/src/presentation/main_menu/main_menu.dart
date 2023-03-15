3ddca38cf3b9adabf8f64872ba42c6d32958bbe2
Luis Felipe Santofimio <luisfelipesantofimio@gmail.com>
2023-03-14 19:17:57 -0500

    Added route to treatment plan screen

--- lib/src/presentation/main_menu/main_menu.dart
+++ lib/src/presentation/main_menu/main_menu.dart
@@ -8,7 +8,9 @@ import 'package:aronnax/src/presentation/core/methods.dart';
 import 'package:aronnax/src/presentation/core/user_global_values.dart';
 import 'package:aronnax/src/presentation/main_menu/menu_options_bar.dart';
 import 'package:aronnax/src/presentation/treatment_plans/treatment_plan_creation/treatment_plan_creation_view.dart';
+import 'package:aronnax/src/presentation/treatment_plans/treatment_plans_view/treatment_plans_view.dart';
 import 'package:aronnax/src/presentation/widgets/calendar_components/events_list_view.dart';
+import 'package:aronnax/src/presentation/widgets/generic_icon_button.dart';
 import 'package:aronnax/src/presentation/widgets/global_calendar.dart';
 import 'package:aronnax/src/presentation/widgets/todo_creation_dialog/todos_creation_dialog.dart';
 import 'package:aronnax/src/presentation/widgets/todos_list_view/todos_list_view.dart';
@@ -66,15 +68,6 @@ class MainMenuState extends ConsumerState<MainMenu> {
                     ),
                     Column(
                       children: [
-                        TextButton(
-                          onPressed: () => Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) =>
-                                    TreatmentPlanCreationView(),
-                              )),
-                          child: Text('Treatment'),
-                        ),
                         Row(
                           crossAxisAlignment: CrossAxisAlignment.center,
                           children: [
@@ -164,6 +157,33 @@ class MainMenuState extends ConsumerState<MainMenu> {
                                 width: MediaQuery.of(context).size.width * 0.25,
                                 child: const GlobalCalendar(),
                               ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            GenericIconButton(
+                              icon: FontAwesomeIcons.book,
+                              title: 'Treatment plans',
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) =>
+                                        const TreatmentPlansView(),
+                                  ),
+                                );
+                              },
+                            ),
+                            GenericIconButton(
+                              icon: FontAwesomeIcons.microscope,
+                              title: 'Tests',
+                              onTap: () {},
+                            ),
+                            GenericIconButton(
+                              icon: FontAwesomeIcons.userGroup,
+                              title: 'Patiens list',
+                              onTap: () {},
                             ),
                           ],
                         ),
