9d0b7d32b1f83e261f6f070eef17653733817904
Luis Felipe Santofimio <luisfelipesantofimio@gmail.com>
2023-03-19 19:04:51 -0500

    Move file from wrong location

---
+++ lib/src/presentation/patient_case_view/patient_case_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class PatientCaseView extends StatelessWidget {
+  const PatientCaseView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
