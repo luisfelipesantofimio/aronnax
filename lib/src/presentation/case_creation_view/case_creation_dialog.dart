526e758202a67e75ea0f0eb6ab4bd88fa9862b53
Luis Felipe Santofimio <luisfelipesantofimio@gmail.com>
2023-03-20 21:21:22 -0500

    Implemented dialog for case creation for patients

---
+++ lib/src/presentation/case_creation_view/case_creation_dialog.dart
@@ -0,0 +1,149 @@
+import 'package:aronnax/src/data/interfaces/patients_repository_interface.dart';
+import 'package:aronnax/src/data/providers/connection_state_provider.dart';
+import 'package:aronnax/src/data/providers/patients_provider.dart';
+import 'package:aronnax/src/data/providers/treatment_plan_providers.dart';
+import 'package:aronnax/src/domain/entities/patient.dart';
+import 'package:aronnax/src/domain/entities/tratment_plan_entities/treatment_plan.dart';
+import 'package:aronnax/src/presentation/case_creation_view/case_creation_form.dart';
+import 'package:aronnax/src/presentation/core/controllers.dart';
+import 'package:aronnax/src/presentation/core/user_global_values.dart';
+import 'package:aronnax/src/presentation/widgets/generic_minimal_button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class CaseCreationDialog extends ConsumerStatefulWidget {
+  const CaseCreationDialog({Key? key, required this.patientData})
+      : super(key: key);
+  final Patient patientData;
+
+  @override
+  ConsumerState<CaseCreationDialog> createState() => _CaseCreationDialogState();
+}
+
+class _CaseCreationDialogState extends ConsumerState<CaseCreationDialog> {
+  bool treatmentPlanActivated = false;
+  @override
+  Widget build(BuildContext context) {
+    final treatmentPlanList = ref.watch(
+      treatmentPlanListProvider(ref.watch(offlineStatusProvider).value!),
+    );
+
+    return Dialog(
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width * 0.4,
+        height: MediaQuery.of(context).size.height * 0.45,
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: ListView(
+            children: [
+              const Text(
+                'New case',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(10),
+              ),
+              const CaseCreationForm(),
+              const Padding(
+                padding: EdgeInsets.all(10),
+              ),
+              Row(
+                children: [
+                  Switch(
+                    value: treatmentPlanActivated,
+                    onChanged: (value) {
+                      setState(() {
+                        treatmentPlanActivated = !treatmentPlanActivated;
+                      });
+                    },
+                  ),
+                  Text(treatmentPlanActivated
+                      ? 'Case with treatment plan'
+                      : 'Case without treatment plan')
+                ],
+              ),
+              const Padding(
+                padding: EdgeInsets.all(10),
+              ),
+              Visibility(
+                visible: treatmentPlanActivated,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Add new treatment plan',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    treatmentPlanList.when(
+                      data: (data) => DropdownButtonFormField(
+                        hint: Text('Select a treatment plan'),
+                        items: data
+                            .map(
+                              (e) => DropdownMenuItem(
+                                value: e,
+                                child: Text(e.title),
+                              ),
+                            )
+                            .toList(),
+                        onChanged: (TreatmentPlan? value) {
+                          ref
+                              .read(caseFormTreatmentPlanProvider.notifier)
+                              .update((state) => value);
+                        },
+                      ),
+                      error: (error, stackTrace) =>
+                          const Text('Something went wrong'),
+                      loading: () => const CircularProgressIndicator(),
+                    ),
+                  ],
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(10),
+              ),
+              GenericMinimalButton(
+                title: 'Save case',
+                onTap: () {
+                  if (caseCreationFormKey.currentState!.validate()) {
+                    ref.read(patientsRepositoryProvider).addPatientCase(
+                        ref,
+                        DateTime.now(),
+                        widget.patientData.id,
+                        ref.read(globalUserInformationProvider)!.id,
+                        ref.read(caseFormConsultationReasonProvider)!,
+                        ref.read(caseFormTreatmentProposalProvider)!,
+                        ref.read(caseFormDiagnosticProvider)!,
+                        ref.read(caseFormCaseNotesProvider),
+                        ref.read(offlineStatusProvider).value!);
+                    if (treatmentPlanActivated) {
+                      ref
+                          .read(patientsRepositoryProvider)
+                          .addPatientTreatmentPlan(
+                            ref,
+                            ref.read(caseFormTreatmentPlanProvider)!.id,
+                            widget.patientData.id,
+                            DateTime.now(),
+                            ref.read(offlineStatusProvider).value!,
+                          );
+                    }
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        backgroundColor: Colors.green,
+                        content: Text('User case saved.'),
+                      ),
+                    );
+                    Navigator.pop(context);
+                    Navigator.pop(context);
+                  }
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/presentation/case_creation_view/case_creation_form.dart
@@ -0,0 +1,80 @@
+import 'package:aronnax/src/data/providers/patients_provider.dart';
+import 'package:aronnax/src/presentation/core/controllers.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class CaseCreationForm extends ConsumerWidget {
+  const CaseCreationForm({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Form(
+      key: caseCreationFormKey,
+      child: Column(
+        children: [
+          TextFormField(
+            maxLines: 3,
+            decoration: const InputDecoration(hintText: 'Motivo de consulta'),
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'El campo no puede estar vacío';
+              }
+            },
+            onChanged: (value) {
+              ref
+                  .read(caseFormConsultationReasonProvider.notifier)
+                  .update((state) => value);
+            },
+          ),
+          const Padding(
+            padding: EdgeInsets.all(10),
+          ),
+          TextFormField(
+            maxLines: 3,
+            decoration: const InputDecoration(hintText: 'Diagnóstico'),
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'El campo no puede estar vacío';
+              }
+            },
+            onChanged: (value) {
+              ref
+                  .read(caseFormDiagnosticProvider.notifier)
+                  .update((state) => value);
+            },
+          ),
+          const Padding(
+            padding: EdgeInsets.all(10),
+          ),
+          TextFormField(
+            maxLines: 3,
+            decoration:
+                const InputDecoration(hintText: 'Propuesta de tratamiento'),
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'El campo no puede estar vacío';
+              }
+            },
+            onChanged: (value) {
+              ref
+                  .read(caseFormTreatmentProposalProvider.notifier)
+                  .update((state) => value);
+            },
+          ),
+          const Padding(
+            padding: EdgeInsets.all(10),
+          ),
+          TextFormField(
+            maxLines: 3,
+            decoration: const InputDecoration(hintText: 'Notas'),
+            onChanged: (value) {
+              ref
+                  .read(caseFormCaseNotesProvider.notifier)
+                  .update((state) => value);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
