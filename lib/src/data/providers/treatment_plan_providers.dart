d2d9fa8e81521ae9c0223e78ca8f60cc28f1b163
Luis Felipe Santofimio <luisfelipesantofimio@gmail.com>
2023-03-29 13:24:27 -0500

    Added providers for responses list and response entity

--- lib/src/data/providers/treatment_plan_providers.dart
+++ lib/src/data/providers/treatment_plan_providers.dart
@@ -1,18 +1,40 @@
 import 'dart:developer';
 import 'package:aronnax/src/data/interfaces/treatment_plans_repository_interface.dart';
 import 'package:aronnax/src/domain/entities/tratment_plan_entities/treatment_plan.dart';
+import 'package:aronnax/src/domain/entities/tratment_plan_entities/treatment_plan_result.dart';
+import 'package:aronnax/src/domain/entities/tratment_plan_entities/treatment_plan_result_value.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final treatmentPlanComponentDecoding =
     StateProvider.family<List<Widget>, String>(
   (ref, componentsList) {
-    log(componentsList);
     List<Widget> decodedWidgets = ref
         .read(treatmentPlanRepositoryProvider)
         .decodeTreatmentComponentData(componentsList);
-    log(decodedWidgets.toString());
+
     return decodedWidgets;
+  },
+);
+
+final currentTreatmentPlanResponseProvider =
+    StateProvider<TreatmentPlanResult?>((ref) => null);
+
+final currentTreatmentPlanResponseListProvider =
+    StateProvider<List<TreatmentPlanResultValue>>(
+  (ref) => [],
+);
+
+final treatmentPlanResultDecoding =
+    StateProvider.family<List<TreatmentPlanResultValue>, String>(
+  (ref, componentsList) {
+    TreatmentPlanResult decodedResult = ref
+        .read(treatmentPlanRepositoryProvider)
+        .decodeTreatmentPlanResults(componentsList);
+
+    final List<TreatmentPlanResultValue> values = decodedResult.results;
+
+    return values;
   },
 );
 
