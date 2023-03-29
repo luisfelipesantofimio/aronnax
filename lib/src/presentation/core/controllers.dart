14801039624e81521ba3f6da2176c8ef86c9a685
Luis Felipe Santofimio <luisfelipesantofimio@gmail.com>
2023-03-29 13:22:19 -0500

    Added form controller to treatment plan session view

--- lib/src/presentation/core/controllers.dart
+++ lib/src/presentation/core/controllers.dart
@@ -6,3 +6,4 @@ final verfiFormkey = GlobalKey<FormState>();
 final clinicHistoryKey = GlobalKey<FormState>();
 final sessionFormKey = GlobalKey<FormState>();
 final caseCreationFormKey = GlobalKey<FormState>();
+final treatmentPlanApplicationFormKey = GlobalKey<FormState>();
