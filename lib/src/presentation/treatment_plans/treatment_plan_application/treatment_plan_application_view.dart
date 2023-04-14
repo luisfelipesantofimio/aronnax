51d589a7496b6f71acf667074b04446724c0306f
Luis Felipe Santofimio <luisfelipesantofimio@gmail.com>
2023-04-14 01:29:17 -0500

    Implemented action buttons for update and saving results

--- lib/src/presentation/treatment_plans/treatment_plan_application/treatment_plan_application_view.dart
+++ lib/src/presentation/treatment_plans/treatment_plan_application/treatment_plan_application_view.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 
+import 'package:aronnax/src/data/interfaces/patients_repository_interface.dart';
 import 'package:aronnax/src/data/interfaces/treatment_plans_repository_interface.dart';
 import 'package:aronnax/src/data/providers/treatment_plan_providers.dart';
 import 'package:aronnax/src/domain/entities/patient_case.dart';
@@ -9,7 +10,7 @@ import 'package:aronnax/src/domain/entities/tratment_plan_entities/treatment_pla
 import 'package:aronnax/src/domain/entities/tratment_plan_entities/treatment_plan_result.dart';
 import 'package:aronnax/src/domain/entities/tratment_plan_entities/treatment_plan_result_value.dart';
 import 'package:aronnax/src/presentation/core/controllers.dart';
-import 'package:aronnax/src/presentation/widgets/treatment_plan_build_widgets/section_list_element.dart';
+import 'package:aronnax/src/presentation/widgets/generic_minimal_button.dart';
 import 'package:aronnax/src/presentation/widgets/treatment_plan_build_widgets/treatment_plan_list_component.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -19,9 +20,13 @@ class TreatmentPlanApplicationView extends ConsumerStatefulWidget {
     Key? key,
     required this.treatmentPlanData,
     required this.caseData,
+    required this.patientSessionAmount,
+    required this.onResultsSaving,
   }) : super(key: key);
   final TreatmentPlan treatmentPlanData;
   final PatientCase caseData;
+  final int patientSessionAmount;
+  final Function(bool resultsSaved) onResultsSaving;
 
   @override
   ConsumerState<TreatmentPlanApplicationView> createState() =>
@@ -31,7 +36,7 @@ class TreatmentPlanApplicationView extends ConsumerStatefulWidget {
 class _TreatmentPlanApplicationViewState
     extends ConsumerState<TreatmentPlanApplicationView> {
   List<Section> sectionList = [];
-  int selectedSectionIndex = 0;
+  int currentTreatmentPhase = 0;
 
   @override
   void initState() {
@@ -43,12 +48,9 @@ class _TreatmentPlanApplicationViewState
       ref.read(currentTreatmentPlanResponseListProvider).clear();
       ref.read(currentTreatmentPlanResponseProvider.notifier).update(
             (state) => TreatmentPlanResult(
-              phaseNumber: widget.treatmentPlanData.sectionsList.indexOf(
-                widget.treatmentPlanData.sectionsList
-                    .firstWhere((element) => element.isComplete == false),
-              ),
+              phaseNumber: widget.caseData.currentTreatmentPlanPhase!,
               applicationDate: DateTime.now(),
-              sessionNumber: 0,
+              sessionNumber: widget.patientSessionAmount + 1,
               patientId: widget.caseData.patientId,
               professionalId: widget.caseData.professionalId,
               treatmentPlanId: widget.treatmentPlanData.id,
@@ -56,27 +58,24 @@ class _TreatmentPlanApplicationViewState
             ),
           );
     });
-    List<TreatmentPlanComponent> componentsList = widget
-        .treatmentPlanData.sectionsList
-        .firstWhere((element) => element.isComplete == false)
-        .components;
+
+    log(ref.read(currentTreatmentPlanResponseListProvider).toString());
+    setState(() {
+      sectionList = widget.treatmentPlanData.sectionsList;
+      currentTreatmentPhase = widget.caseData.currentTreatmentPlanPhase!;
+    });
+    List<TreatmentPlanComponent> componentsList =
+        widget.treatmentPlanData.sectionsList[currentTreatmentPhase].components;
 
     for (var element in componentsList) {
       ref.read(currentTreatmentPlanResponseListProvider.notifier).state.add(
             TreatmentPlanResultValue(
                 componentId: element.id!,
-                treatmentPhase: widget.treatmentPlanData.sectionsList.indexOf(
-                  widget.treatmentPlanData.sectionsList
-                      .firstWhere((element) => element.isComplete == false),
-                ),
+                treatmentPhase: widget.caseData.currentTreatmentPlanPhase!,
                 messurable: element.messurable,
                 value: null),
           );
     }
-    log(ref.read(currentTreatmentPlanResponseListProvider).toString());
-    setState(() {
-      sectionList = widget.treatmentPlanData.sectionsList;
-    });
     super.initState();
   }
 
@@ -95,7 +94,8 @@ class _TreatmentPlanApplicationViewState
         ref
             .read(treatmentPlanRepositoryProvider)
             .encodeTreatmentComponentPlanData(
-              sectionList[selectedSectionIndex].components,
+              sectionList[widget.caseData.currentTreatmentPlanPhase!]
+                  .components,
             ),
       ),
     );
@@ -108,7 +108,7 @@ class _TreatmentPlanApplicationViewState
             return Row(
               children: [
                 Container(
-                  color: Color.fromARGB(255, 240, 239, 239),
+                  color: const Color.fromARGB(255, 240, 239, 239),
                   width: constrains.maxWidth * 0.6,
                   child: Column(
                     children: [
@@ -134,11 +134,116 @@ class _TreatmentPlanApplicationViewState
                 ),
                 SizedBox(
                   width: constrains.maxWidth * 0.4,
-                  child: Column(
-                    children: [
-                      Text(widget.treatmentPlanData.title),
-                      Text(widget.treatmentPlanData.description),
-                    ],
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Column(
+                          children: [
+                            SizedBox(
+                              child: Text(
+                                widget.treatmentPlanData.title,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 30,
+                                ),
+                              ),
+                            ),
+                            Text(widget.treatmentPlanData.description),
+                          ],
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.all(20),
+                        ),
+                        Row(
+                          children: [
+                            GenericMinimalButton(
+                              title: 'Save session results',
+                              onTap: () {
+                                if (treatmentPlanApplicationFormKey
+                                    .currentState!
+                                    .validate()) {
+                                  ref
+                                      .read(
+                                          currentTreatmentPlanResponseProvider)!
+                                      .copyWith(
+                                        results: ref.read(
+                                            currentTreatmentPlanResponseListProvider),
+                                      );
+
+                                  ref
+                                      .read(treatmentPlanRepositoryProvider)
+                                      .saveTreatmentPlanResults(
+                                        ref,
+                                        ref.read(
+                                            currentTreatmentPlanResponseProvider)!,
+                                        widget.caseData,
+                                      );
+                                  widget.onResultsSaving(true);
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                      backgroundColor: Colors.green,
+                                      content:
+                                          Text('Saved treatment plan results!'),
+                                    ),
+                                  );
+                                  Navigator.pop(context);
+                                }
+                              },
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.all(10),
+                            ),
+                            Visibility(
+                              visible:
+                                  widget.treatmentPlanData.sectionsList.length >
+                                      1,
+                              child: GenericMinimalButton(
+                                title: 'Close current phase',
+                                onTap: () {
+                                  if (widget
+                                          .caseData.currentTreatmentPlanPhase! <
+                                      widget.treatmentPlanData.sectionsList
+                                          .length) {
+                                    ref
+                                        .read(patientsRepositoryProvider)
+                                        .updatePatientCaseCurrentPhase(
+                                            ref,
+                                            widget.caseData.id,
+                                            widget.caseData
+                                                    .currentTreatmentPlanPhase! +
+                                                1);
+
+                                    setState(() {
+                                      currentTreatmentPhase = widget.caseData
+                                              .currentTreatmentPlanPhase! +
+                                          1;
+                                    });
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      const SnackBar(
+                                        backgroundColor: Colors.green,
+                                        content: Text(
+                                            'Moved to the next treatment plan phase'),
+                                      ),
+                                    );
+                                    //   Navigator.pop(context);
+                                  } else {
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      const SnackBar(
+                                        backgroundColor: Colors.red,
+                                        content:
+                                            Text('There are no more phases'),
+                                      ),
+                                    );
+                                  }
+                                },
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
                   ),
                 )
               ],
