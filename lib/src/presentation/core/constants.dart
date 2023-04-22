b4d9b0d600b6f6ab53d411ef9d378e3dbc53d5a0
Luis Felipe Santofimio Buitrago <luisfelipesantofimio@gmail.com>
2023-04-21 20:27:18 -0500

    Added enviroment variables for icd api keys

--- lib/src/presentation/core/constants.dart
+++ lib/src/presentation/core/constants.dart
@@ -6,8 +6,11 @@ class AppConstants {
   static const String globalVersion = "0.0.1";
   static const String compilationDate = "12/06/2022";
 
-  //Form controllers
+  //ICD API keys enviroment variables
+  static const clientId = String.fromEnvironment("clientId");
+  static const String clientSecret = String.fromEnvironment("clientSecret");
 }
 
+//Form controllers
 final loginKey = GlobalKey<FormState>();
 final appointmentCreationKey = GlobalKey<FormState>();
