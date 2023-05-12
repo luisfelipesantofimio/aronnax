ae33c2c567e7956008f5abe4fff66f5749490c95
Luis Felipe Santofimio <luisfelipesantofimio@gmail.com>
2023-05-12 18:48:07 -0500

    Added repository for professional logic

---
+++ lib/src/data/repositories/professional_repository.dart
@@ -0,0 +1,53 @@
+import 'dart:convert';
+
+import 'package:aronnax/src/data/interfaces/local_database_interface.dart';
+import 'package:aronnax/src/data/interfaces/professional_repository_interface.dart';
+import 'package:aronnax/src/data/providers/connection_state_provider.dart';
+import 'package:crypt/crypt.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ProfessionalRepository implements ProfessionalRepositoryInterface {
+  @override
+  void createProfessionalProfile({
+    required WidgetRef ref,
+    required String names,
+    required String lastNames,
+    required String userName,
+    required int personalId,
+    required int professionalId,
+    required String adress,
+    required String email,
+    required String countryCode,
+    required String password,
+    required String securityQuestion,
+    required String securityAnswer,
+  }) {
+    final hashedPassword = Crypt.sha256(password);
+    final hashedSecurityAnswer = Crypt.sha256(securityAnswer);
+
+    //  bool isOffline = ref.read(offlineStatusProvider).value!;
+    //   if (isOffline) {
+    ref.read(localDatabaseRepositoryProvider).addLocalProfessional(
+          names: names,
+          lastNames: lastNames,
+          userName: userName,
+          personalId: personalId,
+          professionalId: professionalId,
+          adress: adress,
+          email: email,
+          countryCode: countryCode,
+          password: hashedPassword.hash,
+          securityQuestion: securityQuestion,
+          securityAnswer: hashedSecurityAnswer.hash,
+        );
+
+    //Sets the app as configured so you will be take to the login screen
+    ref.read(localDatabaseRepositoryProvider).updateConfigurationState(true);
+
+    //Configure the app to use the local database
+    ref.read(localDatabaseRepositoryProvider).updateConnectionMode(true);
+    //  }
+  }
+}
+
+//TODO: finish registration system
