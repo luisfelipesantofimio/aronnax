863ee9f590f717ca7f4da7fcab4082d11ea3949d
Luis Felipe Santofimio <luisfelipesantofimio@gmail.com>
2023-05-12 18:50:02 -0500

    Modified routes to professional register

--- lib/src/presentation/welcome_screens/first.dart
+++ lib/src/presentation/welcome_screens/first.dart
@@ -1,4 +1,4 @@
-import 'package:aronnax/src/Pages/Formulary/professionalForm/local_professional_register_view.dart';
+import 'package:aronnax/src/presentation/professional_register/local_professional_register_view.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
--- lib/src/presentation/welcome_screens/second.dart
+++ lib/src/presentation/welcome_screens/second.dart
@@ -1,4 +1,4 @@
-import 'package:aronnax/src/Pages/Formulary/professionalForm/local_professional_register_view.dart';
+import 'package:aronnax/src/presentation/professional_register/local_professional_register_view.dart';
 import 'package:aronnax/src/presentation/welcome_screens/create_server.dart';
 import 'package:aronnax/src/presentation/welcome_screens/server_config.dart';
 import 'package:flutter/material.dart';
