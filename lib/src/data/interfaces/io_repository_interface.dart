2f2c98f7626f4579c639f6e2a8e7fb8922631610
Luis Felipe Santofimio <luisfelipesantofimio@gmail.com>
2023-05-17 01:20:03 -0500

    Added encryption and decryption logic on io repository

--- lib/src/data/interfaces/io_repository_interface.dart
+++ lib/src/data/interfaces/io_repository_interface.dart
@@ -7,6 +7,14 @@ abstract class IoRepositoryInterface {
   Future<File> exportToTextFile(
       {required String fileName, required String contents});
   Future<String> readFromTextFile(String pathToFile);
+  void encryptFile({
+    required File input,
+    required String encryptionKey,
+  });
+  Future<File> decryptFile({
+    required String encryptedFilePath,
+    required String encryptionKey,
+  });
 }
 
 final ioRepositoryProvider = Provider<IoRepositoryInterface>(
--- lib/src/data/repositories/io_repository.dart
+++ lib/src/data/repositories/io_repository.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:aronnax/src/data/interfaces/io_repository_interface.dart';
+import 'package:encrypt/encrypt.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as p;
@@ -29,5 +30,38 @@ class IoRepository implements IoRepositoryInterface {
     );
     File file = File(result!.files.single.path!);
     return await file.readAsString();
+  }
+
+  @override
+  Future<File> decryptFile(
+      {required String encryptedFilePath,
+      required String encryptionKey}) async {
+    final key = Key.fromUtf8(encryptionKey);
+    final iv = IV.fromLength(16);
+    final encrypter = Encrypter(AES(key));
+    final encryptedFile = File(encryptedFilePath);
+    final decryptedContents = encrypter
+        .decryptBytes(Encrypted(encryptedFile.readAsBytesSync()), iv: iv);
+
+    final decryptedFile =
+        File(encryptedFilePath.replaceAll('.encrypted', 'test'));
+    await decryptedFile.writeAsBytes(decryptedContents);
+
+    return decryptedFile;
+  }
+
+  @override
+  void encryptFile({required File input, required String encryptionKey}) {
+    final encryptedFile = File('${input.path}.encrypted');
+
+    final key = Key.fromUtf8(encryptionKey);
+    final iv = IV.fromLength(16);
+
+    final encrypter = Encrypter(AES(key));
+
+    final encryptedBytes =
+        encrypter.encryptBytes(input.readAsBytesSync(), iv: iv).bytes;
+
+    encryptedFile.writeAsBytesSync(encryptedBytes);
   }
 }
