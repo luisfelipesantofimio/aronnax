f1b7213f23a4e8de3610550372dd1a8343e6adf5
Luis Felipe Santofimio Buitrago <luisfelipesantofimio@gmail.com>
2023-06-08 01:39:21 -0500

    Implemented pin form for user account recover

--- lib/src/presentation/password_recover/password_recover_view.dart
+++ lib/src/presentation/password_recover/password_recover_view.dart
@@ -1,4 +1,5 @@
 import 'package:aronnax/src/domain/entities/professional.dart';
+import 'package:aronnax/src/presentation/password_recover/password_pin_recover.dart';
 import 'package:aronnax/src/presentation/password_recover/password_questionary.dart';
 import 'package:flutter/material.dart';
 
@@ -13,13 +14,23 @@ class PasswordRecoverView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: SizedBox(
-                width: MediaQuery.of(context).size.width * 0.5,
+                width: MediaQuery.of(context).size.width * 0.45,
                 child: PasswordQuestionary(professionalData: professionalData)),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.45,
+              child: PasswordPinRecover(
+                professionalData: professionalData,
+              ),
+            ),
           ),
         ],
       ),
