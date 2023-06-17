a8a27a6ca52c9cf14622da1d4699fefcf272b556
Luis Felipe Santofimio Buitrago <luisfelipesantofimio@gmail.com>
2023-06-16 20:28:04 -0500

    Applied dart fixes

--- lib/src/presentation/settings/configOptions/localdb_selection.dart
+++ lib/src/presentation/settings/configOptions/localdb_selection.dart
@@ -1,6 +1,4 @@
-import 'package:aronnax/src/presentation/settings/widgets/no_server_dialog.dart';
 import 'package:aronnax/src/data/database/local_model/local_model.dart';
-import 'package:aronnax/src/data/interfaces/local_database_interface.dart';
 import 'package:aronnax/src/data/providers/connection_state_provider.dart';
 
 import "package:flutter/material.dart";
